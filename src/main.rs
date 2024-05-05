a8b88d75a69ab0e3ccd1c203a43ba13f7255b4e2
Selyss <99344963+Selyss@users.noreply.github.com>
2024-05-05 14:45:11 -0400

    8x8 board rep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug, Clone, Copy)]
+    pub enum PieceType {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King,
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum PieceColor {
+        White,
+        Black,
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Square {
+        piece_type: Option<PieceType>,
+        piece_color: Option<PieceColor>,
+    }
+
+    pub struct Board(pub [[Square; 8]; 8]);
+
+    impl Board {}
 }
